3b92461ee2a0a6c9c0cbab4b4d72bc49e47b1b76
petr <petr2768@yandex.ru>
2021-02-25 04:11:21 +0300

    обмен валют

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -6,8 +6,8 @@ package com.javarush.task.task03.task0303;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(convertEurToUsd(2, 0.5));
         System.out.println(convertEurToUsd(1, 20));
+        System.out.println(convertEurToUsd(2, 50));
 
     }
 
