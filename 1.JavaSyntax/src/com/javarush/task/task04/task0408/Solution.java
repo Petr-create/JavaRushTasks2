8301044d5d641a622135e34c0b08bbb14bf501ba
petr <petr2768@yandex.ru>
2021-02-26 17:20:40 +0300

    хорошо или плохо?

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0408/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task04.task0408;
+
+/* 
+Хорошо или плохо?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        compare(3);
+        compare(6);
+        compare(5);
+    }
+
+    public static void compare(int number) {
+        if (number < 5){
+            //System.out.println(number + " меньше 5");
+            System.out.println("Число меньше 5");
+        } else if (number > 5) {
+            //System.out.println(number + " больше 5");
+            System.out.println("Число больше 5");
+        } else {
+            //System.out.println(number + " равно 5");
+            System.out.println("Число равно 5");
+        }
+
+    }
+}
