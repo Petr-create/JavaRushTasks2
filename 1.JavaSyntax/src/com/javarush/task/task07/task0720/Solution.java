cfe5df6dd7495e2135105a5c1ea100daf194dd22
petr <petr2768@yandex.ru>
2021-03-10 00:47:34 +0300

    перестановочка подоспела

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task07.task0720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Перестановочка подоспела
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(reader.readLine());
+        int M = Integer.parseInt(reader.readLine());
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            arrayList.add(reader.readLine());
+        }
+        for (int i = 0; i < M; i++) {
+            arrayList.add(arrayList.get(0));
+            arrayList.remove(0);
+        }
+        for(String s : arrayList){
+            System.out.println(s);
+        }
+    }
+}
