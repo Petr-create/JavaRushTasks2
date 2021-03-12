58f8eb7d4f526c216f0226ef7dc8ac607cf3a477
petr <petr2768@yandex.ru>
2021-03-12 17:00:44 +0300

    омовениае рамы

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task08.task0823;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/* 
+Омовение Рамы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+        //------------------------------------------
+        //работает только с лат буквами
+//        Pattern pattern = Pattern.compile("\\w+");
+//        Matcher matcher = pattern.matcher(string);
+//        String str = "";
+//        while (matcher.find()) {
+//            str += matcher.group().substring(0, 1).toUpperCase() + matcher.group().substring(1) + " ";
+//        }
+//        System.out.println(str);
+        //------------------------------------------
+
+        StringTokenizer tokenizer = new StringTokenizer(string);
+        String str = "";
+        while(tokenizer.hasMoreTokens()) {
+            String s = tokenizer.nextToken();
+            str += s.substring(0,1).toUpperCase() + s.substring(1) + " ";
+        }
+        System.out.println(str);
+    }
+}
