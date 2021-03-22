863ed20801bb01550cf4089c69b64ed09fbe74f9
petr <petr2768@yandex.ru>
2021-03-22 17:27:11 +0300

    функциональности маловато

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1019/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task10.task1019;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Функциональности маловато!
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Integer> map = new HashMap<>();
+        try {
+            while (true) {
+                int id = Integer.parseInt(reader.readLine());
+                String name = reader.readLine();
+                map.put(name, id);
+            }
+        } catch (IllegalArgumentException e){
+
+        }
+        map.forEach((a, b) -> System.out.println(b + " " + a));
+    }
+}
