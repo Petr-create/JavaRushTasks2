72ab3cc60fd39552bcc431918ee9efbc4e6448aa
petr <petr2768@yandex.ru>
2021-03-31 15:02:30 +0300

    Закрепляем паттерн Singleton

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1522/Planet.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task15.task1522;
+
+public interface Planet {
+    final static String SUN = "sun";
+    final static String MOON = "moon";
+    final static String EARTH = "earth";
+}
