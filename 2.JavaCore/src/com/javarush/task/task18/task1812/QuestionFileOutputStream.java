906a7db57fad8eede55fc018d657589b3a19fad4
petr <petr2768@yandex.ru>
2021-04-15 22:29:56 +0300

    Расширяем AmigoOutputStream

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task18.task1812;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Расширяем AmigoOutputStream
+*/
+
+public class QuestionFileOutputStream implements AmigoOutputStream {
+    AmigoOutputStream stream;
+    public QuestionFileOutputStream(AmigoOutputStream stream){
+        this.stream = stream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+       stream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        stream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        stream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        stream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+        if(string.equals("Д")){
+            stream.close();
+        }
+    }
+}
+
