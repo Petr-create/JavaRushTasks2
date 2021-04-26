90882320003c46ded8558e7f87d576a1e709e2c5
petr <petr2768@yandex.ru>
2021-04-26 20:55:30 +0300

    Ридер обертка 2

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task19.task1912;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка 2
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream printStream = System.out;
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(arrayOutputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String str = arrayOutputStream.toString().replace("te", "??");
+        System.setOut(printStream);
+        System.out.println(str);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
