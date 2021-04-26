0ea72ee977ca4595b474b24ea95e91053dd972cf
petr <petr2768@yandex.ru>
2021-04-26 15:42:38 +0300

    Замена знаков

--- 2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1909/Solution.java
@@ -7,6 +7,21 @@ import java.util.ArrayList;
 Замена знаков
 */
 
+//Считать с консоли 2 имени файла.
+//        Первый Файл содержит текст.
+//        Считать содержимое первого файла и заменить все точки "." на знак "!".
+//        Результат вывести во второй файл.
+//        Закрыть потоки.
+//
+//
+//        Требования:
+//        1. Программа должна считывать имена файлов с консоли (используй BufferedReader).
+//        2. BufferedReader для считывания данных с консоли должен быть закрыт.
+//        3. Программа должна считывать содержимое первого файла (используй BufferedReader c конструктором FileReader).
+//        4. Поток чтения из файла (BufferedReader) должен быть закрыт.
+//        5. Программа должна записывать во второй файл содержимое первого файла, где заменены все точки "." на знак "!" (Для записи в файл используй BufferedWriter с конструктором FileWriter).
+//        6. Поток записи в файл (BufferedWriter) должен быть закрыт.
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         String fileName1;
@@ -15,6 +30,7 @@ public class Solution {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             fileName1 = br.readLine();
             fileName2 = br.readLine();
+
         }
 
 //        try(FileReader fileReader = new FileReader(fileName1);
