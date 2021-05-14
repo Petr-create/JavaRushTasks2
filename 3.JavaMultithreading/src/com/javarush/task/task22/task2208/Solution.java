ac18de943ce0ef30f16a9a001423dd06f5ff48eb
petr <petr2768@yandex.ru>
2021-05-14 23:56:04 +0300

    Формируем WHERE (super)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Формируем WHERE
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", null);
+        map.put("city", "Saint-Petersburg");
+        map.put("age", null);
+        System.out.println(getQuery(map));
+
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder queryBuilder = new StringBuilder();
+        for (String s : params.keySet()) {
+            String value = params.get(s);
+            if (value == null) {
+                continue;
+            }
+            if (queryBuilder.toString().length() != 0) {
+                queryBuilder.append(" and ");
+            }
+            queryBuilder.append(s + " = '" + value + "'");
+        }
+        return queryBuilder.toString();
+    }
+}
