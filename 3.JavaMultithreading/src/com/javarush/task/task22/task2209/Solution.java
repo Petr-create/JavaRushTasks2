0d4f5f616e93ff34db7b9968a007658ee29460dc
petr <petr2768@yandex.ru>
2021-05-19 17:18:18 +0300

    Составить цепочку слов (super n)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -0,0 +1,101 @@
+package com.javarush.task.task22.task2209;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/* 
+Составить цепочку слов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String nameFile = reader.readLine();
+
+        List<String> str = new ArrayList<>();
+
+        try(BufferedReader readerFile = new BufferedReader(new FileReader(nameFile))){
+            while(readerFile.ready()){
+                str.add(readerFile.readLine());
+            }
+        }
+
+        List<String> resultList = new ArrayList<>();
+
+        for(String s : str){
+            StringTokenizer tknzr = new StringTokenizer(s);
+            while(tknzr.hasMoreTokens()){
+                resultList.add(tknzr.nextToken());
+            }
+        }
+
+        StringBuilder result = getLine(getWords(resultList));
+        System.out.println(result.toString());
+    }
+
+    private static String[] getWords(List<String> list) {
+        String[] array = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    public static StringBuilder getLine(String... words) {
+        StringBuilder builder = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            list.add(i);
+            if (findSolutions(list, words)) {
+                for (Integer integer : list) {
+                    builder.append(words[integer]);
+                    builder.append(" ");
+                }
+                return builder;
+            }
+            list.remove(Integer.valueOf(i));
+        }
+
+        return builder;
+    }
+
+    private static boolean findSolutions(List<Integer> list, String... words) {
+        if (list.size() == words.length) {
+            return true;
+        }
+        for (int i = 0; i < words.length; i++) {
+            if (isValid(list, words[i], words)) {
+                list.add(i);
+                if (findSolutions(list, words)) {
+                    return true;
+                }
+                list.remove(Integer.valueOf(i));
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValid(List<Integer> list, String word, String... words) {
+        for (Integer integer : list) {
+            if (words[integer].equals(word)) {
+                return false;
+            }
+        }
+        return isTheSameChars(words[getLastElement(list)], word);
+    }
+
+    private static boolean isTheSameChars(String firstWord, String secondWord) {
+        if (firstWord.endsWith(" ")) {
+            firstWord = firstWord.substring(0, firstWord.length() - 1);
+        }
+        return firstWord.isEmpty() || (secondWord != null &&
+                Character.toUpperCase(firstWord.charAt(firstWord.length() - 1)) == Character.toUpperCase(secondWord.charAt(0)));
+    }
+
+    private static <T> T getLastElement(List<? extends T> list) {
+        return list.get(list.size() - 1);
+    }
+}
